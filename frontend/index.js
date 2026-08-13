41fd209455185a7a70a42da5056de4d460af118d
GOUDA RAKSHITHA <rakshithagouda19@gmail.com>
2026-08-13 17:08:30 +0530

    developed a small application

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,26 @@
-alert("Connected to the server!");
-alert("this is from, javascript");
+//alert("Connected to the server!");
+//alert("this is from, javascript");
+const users=[
+    {
+        "name": "Rakshitha",
+        "gender": "Female",
+        "image": "jane.png"
+    },
+    {
+        "name": "John",
+        "gender": "Male",
+        "image": "john.png"
+    }
+]
+
+var curIndex=0;
+function toggle()
+{
+    if(curIndex==0)
+        curIndex=1;
+    else
+        curIndex=0;
+    document.getElementById("card-image").src=users[curIndex].image;
+    document.getElementById("card-name").innerText=users[curIndex].name;
+    document.getElementById("card-gender").innerText=users[curIndex].gender;
+}
